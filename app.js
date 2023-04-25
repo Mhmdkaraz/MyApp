aa014dc390ee10ef954cf86bee08d1439db62e5f
Mhmdkaraz <mohamedkaraz22@gmail.com>
2023-04-26 00:28:50 +0300

    Added modal on row click Added checkbox to toggle a todo (completed/not)

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 Ext.onReady(function () {
-  var todos = localStorage.getItem('todos')
-    ? JSON.parse(localStorage.getItem('todos'))
-    : [];
-
+  var todos = getTodoItems();
   var todoStore = Ext.create('Ext.data.Store', {
-    fields: ['id', 'description', 'completed'],
+    fields: ['id', 'title', 'description', 'completed'],
     proxy: {
       type: 'localstorage',
       id: 'todos',
@@ -13,10 +10,10 @@ Ext.onReady(function () {
   });
 
   var formPanel = Ext.create('Ext.form.Panel', {
-    title: 'Add Todo',
     itemId: 'formPanel',
-    width: 300,
+    width: 400,
     bodyPadding: 10,
+    cls: 'my-form-panel',
     defaultType: 'textfield',
     items: [
       {
@@ -27,28 +24,33 @@ Ext.onReady(function () {
         allowBlank: true,
       },
       {
+        fieldLabel: 'Title',
+        name: 'title',
+        allowBlank: false,
+        xtype: 'textfield',
+      },
+      {
         fieldLabel: 'Description',
         name: 'description',
         allowBlank: false,
         xtype: 'textarea',
       },
-      {
-        xtype: 'checkbox',
-        fieldLabel: 'Completed',
-        name: 'completed',
-      },
     ],
     buttons: [
       {
-        text: 'Add',
+        text: 'Save',
         itemId: 'addBtn',
         handler: function () {
           var form = this.up('form').getForm();
           if (form.isValid()) {
             var values = form.getValues();
-            // console.log(values)
-            addTodoItem(values);
+            values.id = Ext.id();
+            var callback = () => {
+              gridPanel.getStore().loadData(getTodoItems())
+            };
+            addTodoItem(values, callback);
             form.reset();
+            todoModal.hide();
           }
         },
       },
@@ -61,101 +63,187 @@ Ext.onReady(function () {
           if (form.isValid()) {
             var values = form.getValues();
             updateTodoItem(values.id, values);
-            todoStore.loadRawData(getTodosFromLocalStorage(), false);
+            todoStore.loadRawData(getTodoItems(), false);
             form.reset();
             this.disable();
             formPanel.down('#addBtn').enable();
+            todoModal.hide();
           }
         },
       },
     ],
-  })
+  });
+
+  var todoModal = Ext.create('Ext.window.Window', {
+    modal: true,
+    closable: true,
+    closeAction: 'hide',
+    width: 420,
+    layout: 'fit',
+    items: [formPanel],
+  });
 
   var gridPanel = Ext.create('Ext.grid.Panel', {
     title: 'Todo List',
     width: 600,
     height: 300,
+    margin: '10',
+    cls: 'my-grid-panel',
     store: todoStore,
     columns: [
-      // { header: 'ID', dataIndex: 'id', flex: 1 },
-      { header: 'Description', dataIndex: 'description', flex: 2 },
       {
-        header: 'Completed',
-        dataIndex: 'completed',
+        header: 'Title',
+        dataIndex: 'title',
         flex: 1,
-        xtype: 'booleancolumn',
-        trueText: 'Yes',
-        falseText: 'No',
+        renderer: function (value, metaData, record) {
+          if (record.get('completed')) {
+            return (
+              '<span style="text-decoration: line-through; -webkit-text-stroke-color: black;">' +
+              value +
+              '</span>'
+            );
+          } else {
+            return value;
+          }
+        },
         listeners: {
-          headerclick: function () {
-            this.up('grid').getStore().sort('completed', 'ASC')
+          click: function (grid, item, index, e) {
+            // show the record data in a modal
+            todoModal.setTitle('Todo Details');
+            todoModal.show();
+            var record = todoStore.getAt(index);
+            formPanel.getForm().loadRecord(record);
+            formPanel.down('#addBtn').hide();
+            formPanel.down('#updateBtn').hide();
           },
         },
       },
       {
-        header: 'Action',
+        header: 'Description',
+        dataIndex: 'description',
+        flex: 2,
+        renderer: function (value, metaData, record) {
+          if (record.get('completed')) {
+            return (
+              '<span style="text-decoration: line-through; -webkit-text-stroke-color: black;">' +
+              value +
+              '</span>'
+            );
+          } else {
+            return value;
+          }
+        },
+        listeners: {
+          click: function (grid, item, index, e) {
+            // show the record data in a modal
+            todoModal.setTitle('Todo Details');
+            todoModal.show();
+            var record = todoStore.getAt(index);
+            formPanel.getForm().loadRecord(record);
+            formPanel.down('#addBtn').hide();
+            formPanel.down('#updateBtn').hide();
+          },
+        },
+      },
+      {
+        xtype: 'checkcolumn',
+        header: 'Completed',
+        dataIndex: 'completed',
+        listeners: {
+          checkchange: function (grid, rowIndex, colIndex) {
+            var record = todoStore.getAt(rowIndex);
+            var todoId = record.get('id');
+            console.log(todoId);
+            var todo = record.data;
+            var callback = () => {
+              gridPanel.getStore().loadData(getTodoItems())
+            };
+            handleCompleteChange(todoId, callback);
+          },
+        },
+      },
+      {
         xtype: 'actioncolumn',
+        header: 'Actions',
         flex: 1,
         items: [
           {
+            xtype: 'actioncolumn',
             icon: './images/edit.png',
             handler: function (grid, rowIndex, colIndex) {
+              formPanel.down('#updateBtn').show();
+              formPanel.down('#addBtn').hide();
               var rec = grid.getStore().getAt(rowIndex);
               formPanel.getForm().loadRecord(rec);
-              Ext.getCmp('tabpanel').setActiveTab(0);
+              todoModal.setTitle('Edit Todo');
+              todoModal.show();
               formPanel.down('#updateBtn').enable();
               formPanel.down('#addBtn').disable();
             },
           },
           {
+            xtype: 'actioncolumn',
             icon: './images/trash.png',
             handler: function (grid, rowIndex, colIndex) {
-              var rec = grid.getStore().getAt(rowIndex);
-              var id = rec.get('id');
-              deleteTodoItem(id);
-              grid.getStore().remove(rec);
+              Ext.Msg.confirm(
+                'Delete',
+                'Are you sure you want to delete this todo?',
+                function (btn) {
+                  if (btn == 'yes') {
+                    var rec = grid.getStore().getAt(rowIndex);
+                    deleteTodoItem(rec.get('id'));
+                    grid.getStore().remove(rec);
+                  }
+                }
+              );
             },
           },
         ],
       },
     ],
-    dockedItems: [
-      {
-        xtype: 'toolbar',
-        dock: 'top',
-        items: [
-          {
-            xtype: 'button',
-            text: 'Refresh',
-            handler: function () {
-              gridPanel.getStore().loadData(getTodosFromLocalStorage());
-            },
-          },
-        ],
-      },
-    ],
-  })
+  });
 
-  var tabPanel = Ext.create('Ext.tab.Panel', {
-    renderTo: Ext.getBody(),
-    id: 'tabpanel',
-    activeTab: 0,
+  var container = Ext.create('Ext.container.Container', {
+    layout: {
+      type: 'vbox',
+      align: 'stretch',
+    },
     items: [
       {
-        title: 'Add Todo',
-        items: [formPanel],
-      },
-      {
-        title: 'Todo List',
-        items: [gridPanel],
-        listeners: {
-          tabchange: function (tabPanel, newCard, oldCard) {
-            if (newCard.title === 'Todo List') {
-              window.location.reload();
-            }
-          },
+        xtype: 'container',
+        layout: {
+          type: 'hbox',
+          align: 'center',
         },
+        items: [
+          {
+            xtype: 'component',
+            flex: 1,
+          },
+          {
+            xtype: 'button',
+            text: 'Add Todo',
+            style: 'background-size:contain;',
+            margin: '10 10 0 10',
+            padding: '10 10 10 10',
+            icon: './images/plus.png',
+            handler: function () {
+              formPanel.down('#updateBtn').hide();
+              formPanel.down('#addBtn').show();
+              todoModal.setTitle('Add Todo');
+              todoModal.show();
+              formPanel.down('#addBtn').enable();
+              formPanel.down('#updateBtn').disable();
+            },
+          },
+        ],
       },
+      gridPanel,
     ],
-  })
-})
+  });
+
+  var viewport = Ext.create('Ext.container.Viewport', {
+    layout: 'fit',
+    items: [container],
+  });
+});
--- todoFunctions.js
+++ todoFunctions.js
@@ -1,43 +1,74 @@
 function generateUniqueId() {
-  var timestamp = new Date().getTime();
-  var randomNumber = Math.floor(Math.random() * 1000000);
-  return timestamp + '-' + randomNumber;
+  var timestamp = new Date().getTime()
+  var randomNumber = Math.floor(Math.random() * 1000000)
+  return timestamp + '-' + randomNumber
 }
-function addTodoItem(todoItem) {
-  todoItem.id = generateUniqueId();
-  var todos = localStorage.getItem('todos')
-    ? JSON.parse(localStorage.getItem('todos'))
-    : [];
-  console.warn(todos, todoItem);
-  todos.push(todoItem);
-  localStorage.setItem('todos', JSON.stringify(todos));
+function addTodoItem(todoItem, callback) {
+  todoItem.id = generateUniqueId()
+  todoItem.completed = false
+  var todos = getTodoItems()
+  // console.warn(todos, todoItem);
+  todos.push(todoItem)
+  localStorage.setItem('todos', JSON.stringify(todos))
+  if (callback) {
+    callback()
+  }
 }
 function updateTodoItem(id, updatedTodoItem) {
-  console.log(id);
-  var todos = localStorage.getItem('todos')
-    ? JSON.parse(localStorage.getItem('todos'))
-    : [];
+  // console.log(id)
+  var todos = getTodoItems()
   var todoItemIndex = todos.findIndex(function (todo) {
-    return todo.id == id;
-  });
+    return todo.id == id
+  })
   if (todoItemIndex !== -1) {
-    todos[todoItemIndex] = updatedTodoItem;
-    localStorage.setItem('todos', JSON.stringify(todos));
+    todos[todoItemIndex] = updatedTodoItem
+    localStorage.setItem('todos', JSON.stringify(todos))
   }
 }
 function deleteTodoItem(id) {
-  var todos = localStorage.getItem('todos')
-    ? JSON.parse(localStorage.getItem('todos'))
-    : [];
+  var todos = getTodoItems()
   var todoItemIndex = todos.findIndex(function (todo) {
-    return todo.id == id;
-  });
+    return todo.id == id
+  })
   if (todoItemIndex !== -1) {
-    todos.splice(todoItemIndex, 1);
-    localStorage.setItem('todos', JSON.stringify(todos));
+    todos.splice(todoItemIndex, 1)
+    localStorage.setItem('todos', JSON.stringify(todos))
   }
 }
-
-function getTodosFromLocalStorage() {
-  return JSON.parse(localStorage.getItem('todos')) || [];
+// function completeTodoItem(id, callback) {
+//   var todos = getTodoItems();
+//   var todoItemIndex = todos.findIndex(function (todo) {
+//     return todo.id == id;
+//   });
+//   if (todoItemIndex !== -1) {
+//     todos[todoItemIndex].completed = true;
+//     localStorage.setItem('todos', JSON.stringify(todos));
+//   }
+//    if (callback) {
+//      callback();
+//    }
+// }
+function handleCompleteChange(id, callback) {
+  var todos = getTodoItems()
+  var todoItemIndex = todos.findIndex(function (todo) {
+    return todo.id == id
+  })
+  console.log(todoItemIndex)
+  console.log(todos[todoItemIndex])
+  if (todoItemIndex !== -1) {
+    if (todos[todoItemIndex].completed) {
+      todos[todoItemIndex].completed = false
+    } else {
+      todos[todoItemIndex].completed = true
+    }
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }
+  if (callback) {
+    callback()
+  }
+}
+function getTodoItems() {
+  return localStorage.getItem('todos')
+    ? JSON.parse(localStorage.getItem('todos'))
+    : []
 }
